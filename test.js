63911ebdfe4a653bc18fce0967a00cb6725057db
Linus Unnebäck <linus@folkdatorn.se>
2018-10-14 14:35:51 +0200

    🐛 Free the pointer returned from malloc

--- test.js
+++ test.js
@@ -8,10 +8,10 @@ const wasmModule = new WebAssembly.Module(code)
 const instance = new WebAssembly.Instance(wasmModule, { env: {} })
 
 console.log('instance.exports.malloc(1454860)')
-instance.exports.malloc(1454860)
+const pointer = instance.exports.malloc(1454860)
 
-console.log('instance.exports.free(131088)')
-instance.exports.free(131088)
+console.log(`instance.exports.free(${pointer})`)
+instance.exports.free(pointer)
 
 console.log('instance.exports.malloc(4786736)')
 instance.exports.malloc(4786736)
